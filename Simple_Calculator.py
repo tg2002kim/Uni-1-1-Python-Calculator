63d246c16de50de5c79e35fdb0bc8168e870d9fb
Kim Taegyung <tg2002kim@dankook.ac.kr>
2025-01-03 20:43:58 +0900

    Update: check if divide by 0

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -2,20 +2,30 @@ val1 = 23
 val2 = 5
 
 # addition
-print("The addition of", val1, "and", val2, "is: ", val1+val2)
+print("The addition of", val1, "and", val2, "is: ", val1 + val2)
 
 # subtraction
-print("The subtraction of", val1, "and", val2, "is: ", val1-val2)
+print("The subtraction of", val1, "and", val2, "is: ", val1 - val2)
 
 # multiplication
-print("The multiplication of", val1, "and", val2, "is: ", val1*val2)
+print("The multiplication of", val1, "and", val2, "is: ", val1 * val2)
 
 # division
-print("The division of", val1, "and", val2, "is: ", val1/val2)
-print("The whole division of", val1, "and", val2, "is: ", val1//val2)
+print("The division of", val1, "and", val2, "is: ", val1 / val2)
+print("The whole division of", val1, "and", val2, "is: ", val1 // val2)
 
 # exponential
-print("The exponential of", val1, "and", val2, "is: ", val1**val2)
+print("The exponential of", val1, "and", val2, "is: ", val1 ** val2)
 
 # modulus
-print("The modulud of", val1, "and", val2, "is: ", val1%val2)
+print("The modulus of", val1, "and", val2, "is: ", val1 % val2)
+
+# Checking if val1 is divisible by val2
+if val1 % val2 == 0:
+    print(f"{val1} is completely divisible by {val2}.")
+else:
+    print(f"{val1} is not completely divisible by {val2}. The remainder is {val1 % val2}.")
+
+
+
+
